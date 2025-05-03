41bbe811d84b62f426a1d910ef9bd771fd240f98
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-05-03 17:04:30 -0400

    pull data from home_page

--- AI_Flashcard/src/FlashCardPage/FlashCardPage.jsx
+++ AI_Flashcard/src/FlashCardPage/FlashCardPage.jsx
@@ -3,10 +3,14 @@ import "./FlashCardPage.css"
 import Web_Title from "../HomePage/Web_Title"
 import FlashCard from "./FlashCard.jsx"
 import KeyTerm from "./KeyTerm.jsx"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 function FlashCardPage(){
-    
+    const location = useLocation();
+    const data = location.state?.data
+
+    console.log(data); //[ {term: 'Iron Man', definition: '..'}, {term: 'Spider-Man', definition: 'Peter Parke..'}
+
 
     return(
         <div>
