8fbf5658fea9b88125987ec20871023162b0b966
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-05-03 15:14:56 -0400

    add browserrouter

--- AI_Flashcard/src/App.jsx
+++ AI_Flashcard/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Home_Page from './HomePage/Home_Page.jsx'
+import { Routes, Route } from 'react-router-dom'
 import FlashCardPage from './FlashCardPage/FlashCardPage.jsx';
 
 import { useState } from 'react';
@@ -10,7 +11,10 @@ function App() {
 
   return (
     <div>
-      <FlashCardPage></FlashCardPage>
+      <Routes>
+        <Route path={'/'} element={<Home_Page/>}></Route>
+        <Route path={'/flashcard'} element={<FlashCardPage/>}></Route>
+      </Routes>
     </div>
   );
 }
