1d370c32d2951ac2810e14516c6ae8a882c1bb3f
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-05-03 16:55:05 -0400

    add loading

--- AI_Flashcard/src/HomePage/Home_Page.jsx
+++ AI_Flashcard/src/HomePage/Home_Page.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import "./Home_Page.css"
 import Web_Title from "./Web_Title";
+import LoadingGif from "../assets/loading-circle.gif"
 
 function Home_Page(){
+    const [isFetching, setIsFetching] = useState(false);
     const [userInput, setUserInput] = useState("")
     const navigate = useNavigate();
     const handleSubmitFile = async (selectedFile) => {
+        setIsFetching(true);
         const formData = new FormData();
         formData.append("file", selectedFile);
         try {
@@ -26,6 +29,7 @@ function Home_Page(){
         }
     };
     const handleSubmitText = async () => {
+        setIsFetching(true)
         const response = await fetch('http://localhost:5000/api/generate-by-topic', {
             method: "POST",
             headers: {"Content-Type": "application/json"},
@@ -36,24 +40,36 @@ function Home_Page(){
         console.log(data);
     }
     return( 
-    <div>
+    <div className='home-page'>
         <Web_Title/>
+        { !isFetching ? 
+            <>
+                {/* <button className = "File-Button">Submit A File</button> */}
+                <label htmlFor="file-upload" className="File-Button">
+                    Submit Upload File
+                </label>
+                <input id="file-upload" type="file" style={{ display: 'none' }} onChange={e => {
+                    const selectedFile = e.target.files[0];
+                    handleSubmitFile(selectedFile);
+                }} />
 
-        {/* <button className = "File-Button">Submit A File</button> */}
-        <label htmlFor="file-upload" className="File-Button">
-            Submit Upload File
-        </label>
-        <input id="file-upload" type="file" style={{ display: 'none' }} onChange={e => {
-            const selectedFile = e.target.files[0];
-            handleSubmitFile(selectedFile);
-        }} />
 
+                <div className = "Text-Container">
+                    <textarea className = "Text-Input" placeholder = "Enter a text or generate something new" rows = "10" value={userInput} onChange={e => setUserInput(e.target.value)}/>
+                </div>
 
-        <div className = "Text-Container">
-            <textarea className = "Text-Input" placeholder = "Enter a text" rows = "25" value={userInput} onChange={e => setUserInput(e.target.value)}/>
-        </div>
+                <button className = "Enter-Button" onClick={handleSubmitText}>Enter</button>
+            </>
+        :   <>
+                    <div className='fetching-content'>
 
-        <button className = "Enter-Button" onClick={handleSubmitText}>Enter</button>
+                        <h1>
+                            Generating Content
+                        </h1>
+                        <img className='loading-circle' src={LoadingGif}></img>
+                    </div>
+            </>
+        }
     </div>
     );
 }
