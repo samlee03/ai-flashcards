c127277db76f8e74969cdf3fe815a8ea9743ab81
Kelvinc9654 <KelvinC9654@gmail.com>
2025-05-03 16:54:01 -0400

    Added a flip card animation

--- AI_Flashcard/src/FlashCardPage/FlashCard.jsx
+++ AI_Flashcard/src/FlashCardPage/FlashCard.jsx
@@ -1,12 +1,33 @@
 
 import "./FlashCard.css"
+import { useState } from "react";
+
 
 function FlashCard(){
+    const [showDefinition, setShowDefinition] = useState(false);
+
+    const handleFlashCardClick = () => {
+        setShowDefinition(prevShowDefinition => !prevShowDefinition);
+    };
+
+
     return(
         <div className = "flash-card-container">
-            <div className = "flash-card">
-                <div className = "term">Some Term</div>
-                <div className = "defintion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus harum mollitia, quo est fugit incidunt atque, cupiditate qui nisi quia itaque nulla asperiores architecto hic commodi! Rem ullam totam officiis?</div>
+            <div className = {`flash-card ${showDefinition ? "Definition" : ""}`} 
+                 onClick = {handleFlashCardClick}>
+
+                <div className = "flash-card-flip">
+
+                    <div className = "Term-Side">
+                        <div className = "term">Some Term</div>
+                    </div>
+
+                    <div className = "Definition-Side"> 
+                        <div className = "definition">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus harum mollitia, quo est fugit incidunt atque, cupiditate qui nisi quia itaque nulla asperiores architecto hic commodi! Rem ullam totam officiis?</div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );
