2cc1514c786d1259635aa26ba562d75be3df026e
Kelvinc9654 <KelvinC9654@gmail.com>
2025-05-03 15:14:20 -0400

    Updated FlashCardPage

--- AI_Flashcard/src/FlashCardPage/KeyTerm.jsx
+++ AI_Flashcard/src/FlashCardPage/KeyTerm.jsx
@@ -5,8 +5,8 @@ function KeyTerm(){
 
     return(
         <div className = "KeyTerm">
-            <div className = "term">Term</div>
-            <div className = "definition">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias suscipit adipisci odit! Autem obcaecati quas magnam aliquid, incidunt est quos corporis neque sit ullam alias aut, laboriosam, cum tempora distinctio?</div>
+            <div className = "Term-Table">Term</div>
+            <div className = "Definition-Table">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias suscipit adipisci odit! Autem obcaecati quas magnam aliquid, incidunt est quos corporis neque sit ullam alias aut, laboriosam, cum tempora distinctio?</div>
         </div>
     )
 }
