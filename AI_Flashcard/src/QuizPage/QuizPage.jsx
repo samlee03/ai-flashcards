79a0ab4c2f74c4b32eeedfe296ede6ff3b63f514
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-05-03 23:22:11 -0400

    merge quizpage

--- AI_Flashcard/src/QuizPage/QuizPage.jsx
+++ AI_Flashcard/src/QuizPage/QuizPage.jsx
@@ -5,7 +5,30 @@ import Web_Title from "../HomePage/Web_Title"
 
 
 function QuizPage({data}){
-
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+    
+    function getNextQuestion() {
+        if (data.length === 0) return null;
+        const index = Math.floor(Math.random() * data.length);
+        const selected = data.splice(index, 1)[0];
+        const incorrectDefs = shuffle(data.map(d => d.definition)).slice(0, 3);
+        const options = shuffle([selected.definition, ...incorrectDefs]);
+        return {
+            term: selected.term,
+            options,
+            correctAnswer: selected.definition
+        };
+    }
+    
+    function resetData() {
+        data = [...originalData]; 
+    }
     return(
         <div>
             <Web_Title/>
